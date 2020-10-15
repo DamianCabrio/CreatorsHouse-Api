44e8fa4ebd7bb67a032a0d01e8303d372daa1c10
n0rb3r7 <n0rb3r7@outlook.com>
2020-10-15 20:59:53 -0300

    Creación UserController
    Funciones index, show, store, update y delete creadas dentro de UserController.

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserCotroller extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+    public function index()
+    {
+        //
+    }
+    public function show(User $user)
+    {
+        //
+    }
+    public function store(Request $request)
+    {
+        
+    }
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+    public function delete(User $user)
+    {
+        //
+    }
+    //
+}
