281719d850bca0d5759acff1cf1f02912fc1353f
Laura <51368962+xlxm01@users.noreply.github.com>
2020-10-19 19:00:52 -0300

    Correccion de error Middleware
    En Auth0Middleware,  Se corrigio la ruta de AUTH0_DOMAIN se saco '/'

--- app/Http/Middleware/Auth0Middleware.php
+++ app/Http/Middleware/Auth0Middleware.php
@@ -33,7 +33,7 @@ class Auth0Middleware
     public function validateToken($token)
     {
         try {
-            $jwksUri = env('AUTH0_DOMAIN') . '.well-known/jwks.json';
+            $jwksUri = env('AUTH0_DOMAIN'). '.well-known/jwks.json';
             $jwksFetcher = new JWKFetcher(null, [ 'base_uri' => $jwksUri ]);
             $signatureVerifier = new AsymmetricVerifier($jwksFetcher);
             $tokenVerifier = new TokenVerifier(env('AUTH0_DOMAIN'), env('AUTH0_AUD'), $signatureVerifier);
