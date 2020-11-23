f8b36d842b85934e422ca8ea82ba6d0263dbcf30
Damián Andrés Cabrio <damiancabrio@hotmail.com>
2020-11-23 17:13:41 -0300

    Se agrego id a la table Follow

--- database/migrations/2020_11_02_215916_create_follow_table.php
+++ database/migrations/2020_11_02_215916_create_follow_table.php
@@ -14,6 +14,7 @@ class CreateFollowTable extends Migration
     public function up()
     {
         Schema::create('follow', function (Blueprint $table) {
+            $table->id();
             $table->foreignId("idUser");
             $table->foreignId("idCreator");
             $table->boolean("isVip");
